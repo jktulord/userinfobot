fb5427be40ba6239f108bce7dd29635b90de0b33
jktulord <olden112002@mail.ru>
2019-08-06 16:48:59 +0300

    fix

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import telebot
-from db import set_state, get_state
 import os
 from flask import Flask, request
 
@@ -14,9 +13,10 @@ def send_welcome(message):
     bot.send_message(chat_id=message.chat.id, text=f'Last Name:{message.chat.last_name}')
     bot.send_message(chat_id=message.chat.id, text=f'Id:, {message.chat.id}')
 
-@bot.message_handler(func=lambda message: true)
+
+@bot.message_handler(func=lambda message: True)
 def send_welcome(message):
-    if message.forward_from == none:
+    if message.forward_from is None:
         bot.send_message(chat_id=message.chat.id, text=f'First Name:, {message.chat.first_name}')
         bot.send_message(chat_id=message.chat.id, text=f'Last Name:{message.chat.last_name}')
         bot.send_message(chat_id=message.chat.id, text=f'Id:, {message.chat.id}')
@@ -25,13 +25,15 @@ def send_welcome(message):
         bot.send_message(chat_id=message.chat.id, text=f'Last Name:{message.forward_from.last_name}')
         bot.send_message(chat_id=message.chat.id, text=f'Id:, {message.forward_from.id}')
 
-@server.route('/' + API_TOKEN, methods=['POST'])
-    def get_message():
-        json_update = request.stream.read().decode('utf-8')
-        update = telebot.types.Update.de_json(json_update)
 
-        bot.process_new_updates([update])
-        return '', 200
+@server.route('/' + API_TOKEN, methods=['POST'])
+def get_message():
+    json_update = request.stream.read().decode('utf-8')
+    update = telebot.types.Update.de_json(json_update)
+
+    bot.process_new_updates([update])
+    return '', 200
+
 
 bot.remove_webhook()
 bot.set_webhook(url=os.getenv('WEBHOOK_URL') + API_TOKEN)
